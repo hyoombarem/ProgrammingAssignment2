967cfcb052678aaba09543cd2cd3158fd7d89cc7
hyoombarem <hahaha420000@gmail.com>
2018-10-30 10:10:48 +0900

    add more comments of introduction of functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions make a matrix to inversed matrix
 
 ## Write a short comment describing this function
 
