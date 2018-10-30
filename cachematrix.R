a95b705e00c36a01e7a47edfa0aba481bdb86f6e
hyoombarem <hahaha420000@gmail.com>
2018-10-30 10:03:30 +0900

    add comments of introduction of functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,10 @@
 
 ## Write a short comment describing this function
 
+
+## This function contains original matrix x and inversed matrix I
+## This return four function that can contact to x and I
+
 makeCacheMatrix <- function(x = matrix()) {
     I <- NULL
     
@@ -18,6 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+
+
+## Function "cacheSolve" get returned value of function "makeCacheMatrix"
+## It returns inversed matrix x that is in the function "makeCacheMatrix"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
